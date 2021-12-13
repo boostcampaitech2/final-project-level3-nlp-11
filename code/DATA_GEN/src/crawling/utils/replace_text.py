7795602615c6bbc7cba39ebaee97a255a7f0062c
root <mon823@naver.com>
2021-12-13 15:32:08 +0900

    [add] update replace regex pattern

--- code/DATA_GEN/src/crawling/utils/replace_text.py
+++ code/DATA_GEN/src/crawling/utils/replace_text.py
@@ -6,39 +6,45 @@ class ReplaceText:
         pass
 
     def get_convert_text(self, context):
-        context = context.replace("\n", "")
-        context = context.replace("&nbsp;", "")
-        # 해쉬태그 and @태그 제거
-        context = re.sub(r"""(#|@)[ㄱ-ㅎ|ㅏ-ㅣ|가-힣a-zA-Z]*""", "", context)
-        # 공백 2개 한개로 변경
-        context = re.sub(r""" +(?= )""", "", context)
-        # URL
-        context = re.sub(
-            r"""https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)""",
-            "",
-            context,
-        )
-        # email
-        context = re.sub(
-            r"""([0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3})""",
-            "",
-            context,
-        )
-        # 전화번호
-        context = re.sub(r"""\d{3}-\d{3,4}-\d{4}""", "", context)
-        # dict 형식 제거
-        context = re.sub(r"""{[^}]*}""", " ", context)
-        # 온점 후 띄어쓰기 강제
-        # context = re.sub(r"""\.(?=[^ ])""", ". ", context)
-        # 아래 나온 문자외 전부 제거
-        context = re.sub(r"""[^0-9a-zA-Zㄱ-ㅎ|ㅏ-ㅣ|가-힣 ().,?!]""", "", context)
-        # ㅋㅋ,ㅎㅎ,ㅜㅜ,ㅠㅠ,..,!!,??,~~ 2개로 제한
-        context = re.sub(r"""(ㅋ[ㅋ]+)""", "ㅋㅋ", context)
-        context = re.sub(r"""(ㅎ[ㅎ]+)""", "ㅎㅎ", context)
-        context = re.sub(r"""(ㅜ[ㅜ]+)""", "ㅜㅜ", context)
-        context = re.sub(r"""(ㅠ[ㅠ]+)""", "ㅠㅠ", context)
-        context = re.sub(r"""(\.[.]+)""", ".. ", context)
-        context = re.sub(r"""(![!]+)""", "! ", context)
-        context = re.sub(r"""(\?[?]+)""", "? ", context)
-        context = re.sub(r"""(~[~]+)""", "~~ ", context)
+
+        context_pattern = {
+            r"""&nbsp;""": "",
+            r"""\u200b""": "",
+            r"""…""": "...",
+            r"""\ufeff""": "",
+            r""" +(?= )""": "",
+            r"""\(Google 번역 제공\)""": "",
+            r"""\(원문\).*""": "",
+            r"""([0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3})""": "",
+            r"""https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)""": "",
+            r"\<저작권자(\(c\)|ⓒ|©|\(Copyright\)|(\(c\))|(\(C\))).+?\>": "",
+            r"저작권자\(c\)|ⓒ|©|(Copyright)|(\(c\))|(\(C\))": "",
+            r"\(출처 ?= ?.+\) |\(사진 ?= ?.+\) |\(자료 ?= ?.+\)| \(자료사진\) |사진=.+기자 ": "",
+            r"""\d{3}-\d{3,4}-\d{4}""": "",
+            r"""{[^}]*}""": "",
+            r"""(#|@)[ㄱ-ㅎ|ㅏ-ㅣ|가-힣a-zA-Z]*""": "",
+            r"""(ㅋ[ ㅋ]+)""": "ㅋㅋ",
+            r"""(ㅎ[ ㅎ]+)""": "ㅎㅎ",
+            r"""(ㅜ[ ㅜ]+)""": "ㅜㅜ",
+            r"""(ㅠ[ ㅠ]+)""": "ㅠㅠ",
+            r"""(\.[ .]+)""": "..",
+            r"""(![ !]+)""": "!!",
+            r"""(\?[ ?]+)""": "??",
+            r"""(~[ ~]+)""": "~~",
+            r"""[^0-9a-zA-Zㄱ-ㅎ|ㅏ-ㅣ|가-힣 ().,?!]""": "",
+            r"""\n""": "",
+        }
+
+        for i in context_pattern.keys():
+            try:
+                context = re.sub(i, context_pattern[i], context)
+            except:
+                context
+        if type(context) == type(0.0):
+            return None
+
+        tmp = re.findall(r"""[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]""", context)
+        if not tmp:
+            return None
+
         return context

bbed11221fb171e3fd035e118599afa605c29bca
root <mon823@naver.com>
2021-12-17 05:08:06 +0900

    [add] add spacing model

--- install/install_requirements.sh
+++ install/install_requirements.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
 pip install python-dotenv==0.19.2
-python3 -m pip install selenium
-python3 -m pip install bs4
-python3 -m pip install tqdm
+pip install selenium
+pip install bs4
+pip install tqdm
+pip install git+https://github.com/haven-jeon/PyKoSpacing.git
+
+# FE
+
+pip install streamlit==1.1.0
+pip install albumentations==1.1.0
+pip install torch==1.10.0
+pip install torchvision==0.11.1
+pip install efficientnet-pytorch==0.7.1
+pip install PyYAML==6.0
+
+# Model
+pip install elasticsearch

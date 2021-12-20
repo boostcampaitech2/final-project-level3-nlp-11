c617546c25ced975232a7880f2314598da262b60
root <mon823@naver.com>
2021-12-20 21:36:17 +0900

    [feat] fix no image image

--- code/FE/src/components/main_list_item.py
+++ code/FE/src/components/main_list_item.py
@@ -9,7 +9,7 @@ env_path = os.path.expanduser("~/final-project-level3-nlp-11/code/.env")
 load_dotenv(dotenv_path=env_path, verbose=True)
 
 
-def main_list_item(data, area):
+def main_list_item(data, area, user_query=None):
     root_path = os.getenv("ROOT_PATH")
 
     spot = data["spot"]
@@ -18,8 +18,10 @@ def main_list_item(data, area):
     addr = data["addr1"]
     tel = data["tel"]
     context = context.replace("\n", "<br>")
-    img_rul = data["img_url"]
-
+    img_url = data["img_url"]
+    style_img = ""
+    if img_url == "https://i.imgur.com/wbN96ze.png":
+        style_img = "opacity:0.5;border:1px solid rgb(29,51,63,.4);padding-top:45px;padding-bottom:45px"
     query = urllib.parse.quote_plus(addr + " " + spot)
     if tel == "전화 정보 없음":
         tmp = f"<b>Tel</b>: {tel}"
@@ -30,12 +32,12 @@ def main_list_item(data, area):
     return f"""
             <div style="clear:both;padding:10px;padding-top:20px">
                 <div id=context_img >
-                    <a href="{root_path}?page=2&target={spot}&location={area}" target="_blank">
-                        <img src="{img_rul}" width="100%" style="border-radius:10px;vertical-align:middle" >
+                    <a href="{root_path}?page=2&target={spot}&location={area}&query={user_query}" target="_blank">
+                        <img src="{img_url}" width="100%" style="border-radius:10px;vertical-align:middle;{style_img}" >
                     </a>
                 </div>
                 <div id=context >
-                    <a href="{root_path}?page=2&target={spot}&location={area}"target="_blank"style="text-decoration:none;color:rgb(29,51,63)">
+                    <a href="{root_path}?page=2&target={spot}&location={area}&query={user_query}"target="_blank"style="text-decoration:none;color:rgb(29,51,63)">
                         <p style="font-size:38px;font-weight:bold;">
                             {spot}
                         </p>
@@ -56,11 +58,11 @@ def main_list_item(data, area):
             </div>"""
 
 
-def parser_data(data, area):
+def parser_data(data, area, user_query=None):
     if not data:
         return ""
     html = ""
     for i in data:
-        html += main_list_item(i, area)
+        html += main_list_item(i, area, user_query)
 
     return html
